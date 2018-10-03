fe535d95156ca0348346663f4a33a1871be7eacb
WM - Work Desktop <jt.bethany@gmail.com>
2018-10-03 14:12:51 -0400

    BV1.85

--- bin/push_html.sh
+++ bin/push_html.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-
-ls
-if $(git pull) == "Already up-to-date."; then
-  echo "No updates to install! Did you push changes to the master branch?"
-  exit
-fi
-
-
 cp -R html /var/www/html
