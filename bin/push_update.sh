e525a54421e0286816701432e9491db5d7533e9e
WM - Work Desktop <jt.bethany@gmail.com>
2018-10-03 14:48:58 -0400

    BV1.95

--- bin/push_update.sh
+++ bin/push_update.sh
@@ -2,9 +2,9 @@
 
 if [ "$(git pull)" == "Already up-to-date." ]; then
   echo "No updates to install! Did you push changes to the master branch?"
-  exit
+#  exit
 fi
 ls -l
 ./fog/src/bin/add_scripts_to_init.bash fog
-./fog/src/bin/install_inits.bash fogbert
+./fog/src/bin/install_inits.bash fog
 ./bin/push_html.sh
