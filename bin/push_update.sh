e17eed6b915cfa1263943f316fb5ac49dd08239a
WM - Work Desktop <jt.bethany@gmail.com>
2018-10-03 13:07:30 -0400

    BV1.6 - finishing update scripts

--- bin/push_update.sh
+++ bin/push_update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd ..
-git pull
+if $(git pull) == "Already up-to-date."; then
+  echo "No updates to install! Did you push changes to the master branch?"
+  exit
+fi
 
 ./fog/src/bin/add_scripts_to_init.bash ..
 ./fog/src/bin/install_inits.bash ..
